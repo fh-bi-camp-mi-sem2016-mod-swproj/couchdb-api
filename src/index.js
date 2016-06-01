2dd23c1c7803cfc313be151c3cd6fc743a9127c9
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-06-01 20:07:22 +0200

    Remove the dummy class HelloWorldDB

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 var DaoHandler = require("./DaoHandler");
 var UserDAO = require("./UserDAO");
 
-var HelloWorldDB = function() {};
-
-HelloWorldDB.prototype.sayHelloWorld = function() {
-    return "Hello, World!";
-};
-
-HelloWorldDB.prototype.sayHello = function(name) {
-    return "Hello, " + name + "!";
-};
-
 var foobar = function() {};
 
-foobar.HelloWorldDB = HelloWorldDB;
 foobar.UserDAO = UserDAO;
 foobar.DaoHandler = DaoHandler;
 
