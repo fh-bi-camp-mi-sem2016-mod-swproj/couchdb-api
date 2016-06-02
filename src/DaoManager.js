c6b5b08ff31a6bbbb36c80564c05c18584e41577
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-06-02 20:57:00 +0200

    Add method to compound the full URL

--- src/DaoManager.js
+++ src/DaoManager.js
@@ -4,6 +4,22 @@ var DaoManager = function(connectionProperties) {
 
 DaoManager.prototype._daos = {};
 
+DaoManager.prototype.connection = {};
+
+DaoManager.prototype.connection.getFullUrl = function() {
+    var result = "";
+
+    result += this.connection.protocol ? this.connection.protocol + "://" : "https://";
+    result += this.connection.user ? this.connection.user : "";
+    result += this.connection.user && this.connection.password ? ":" + this.connection.password : "";
+    result += this.connection.user ? "@" : "";
+    result += url;
+    result += this.connection.port ? ":" + this.connection.port : "";
+    result += "/" + this.connection.database + "/";
+
+    return result;
+};
+
 DaoManager.prototype.getDao = function(dao) {
     if (!this._daos[dao]) {
         this._daos[dao] = createDao(dao);
