f77d8a8ff0ace8aecf085d7d6aa14b723aacade6
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-06-06 22:13:10 +0200

    Move the connection property into constructor

--- src/DaoManager.js
+++ src/DaoManager.js
@@ -1,4 +1,27 @@
 var DaoManager = function(connectionProperties) {
+    this._daos = {};
+    this.connection = {
+        protocol: "https",
+        user: "",
+        password: "",
+        url: "", // Mandatory
+        port: "",
+        database: "" // Mandatory
+    };
+    this.connection.getFullUrl = function() {
+        var result = "";
+
+        result += this.protocol ? this.protocol + "://" : "https://";
+        result += this.user ? this.user : "";
+        result += this.user && this.password ? ":" + this.password : "";
+        result += this.user ? "@" : "";
+        result += this.url;
+        result += this.port ? ":" + this.port : "";
+        result += "/" + this.database + "/";
+
+        return result;
+    };
+
     if (connectionProperties) {
         if (connectionProperties.protocol) {
             this.connection.protocol = connectionProperties.protocol;
@@ -25,31 +48,6 @@ var DaoManager = function(connectionProperties) {
     } else {
         console.error("No connection properties given!");
     }
-};
-
-DaoManager.prototype._daos = {};
-
-DaoManager.prototype.connection = {
-    protocol: "https",
-    user: "",
-    password: "",
-    url: "", // Mandatory
-    port: "",
-    database: "" // Mandatory
-};
-
-DaoManager.prototype.connection.getFullUrl = function() {
-    var result = "";
-
-    result += this.connection.protocol ? this.connection.protocol + "://" : "https://";
-    result += this.connection.user ? this.connection.user : "";
-    result += this.connection.user && this.connection.password ? ":" + this.connection.password : "";
-    result += this.connection.user ? "@" : "";
-    result += this.connection.url;
-    result += this.connection.port ? ":" + this.connection.port : "";
-    result += "/" + this.connection.database + "/";
-
-    return result;
 };
 
 DaoManager.prototype.getDao = function(dao) {
