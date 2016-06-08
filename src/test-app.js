fc322fd849ebdbba72b05320051f0c85dd472e82
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-06-08 16:12:12 +0200

    Add test code to delete a document

--- src/test-app.js
+++ src/test-app.js
@@ -18,3 +18,14 @@ var callbacks = {
 };
 
 userDao.findAll(callbacks);
+
+// var myUser = {
+//     _id: "ca5c2c9fb2d201991f8b6f06e62440e0",
+//     _rev: "1-72f83619c7de8f9d9517db3ab5e445fb",
+//     doctype: "user",
+//     login: "carmenelectra",
+//     password: "carmenelectra",
+//     role: 0
+// };
+//
+// userDao.delete(myUser);
