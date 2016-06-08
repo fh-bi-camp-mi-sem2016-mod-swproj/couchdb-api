f9cb22cd32228b7d2538b58669cd1af24398202f
dbene <bene.strz@googlemail.com>
2016-06-08 12:37:07 +0200

    added pictureDao

---
+++ src/PictureDAO.js
@@ -0,0 +1,37 @@
+var DaoHelper = require("./DaoHelper")
+
+var PictureDAO = function(connection) {
+    this.connection = connection;
+    this.daoHelper = new DaoHelper();
+};
+
+PictureDAO.prototype.findByProfile = function(profile, callbacks) {
+    this.daoHelper.find(this.connection.getFullUrl() + "_design/picture/_view/picForProfile?key=[%22" + encodeURI(profile) + "%22]", callbacks);
+};
+
+PictureDAO.prototype.findAll = function(callbacks) {
+    this.daoHelper.find(this.connection.getFullUrl() + "_design/picture/_view/picALL", callbacks);
+};
+
+PictureDAO.prototype.create = function(obj, callbacks) {
+    this.daoHelper.create(obj, this.connection.getFullUrl() + "", callbacks);
+};
+
+PictureDAO.prototype.update = function(obj, callbacks) {
+    this.daoHelper.update(obj, this.connection.getFullUrl() + obj._id, callbacks);
+};
+
+PictureDAO.prototype.createOrUpdate = function(obj, callbacks) {
+    if (obj._id) {
+        this.update(obj, this.connection.getFullUrl() + obj_id, callbacks);
+    } else {
+        this.create(obj, this.connection.getFullUrl() + "", callbacks);
+    }
+};
+
+PictureDAO.prototype.delete = function(obj, callbacks) {
+    this.daoHelper.delete(obj, this.connection.getFullUrl() + obj_id, callbacks);
+};
+
+exports.default = PictureDAO;
+module.exports = exports.default;
