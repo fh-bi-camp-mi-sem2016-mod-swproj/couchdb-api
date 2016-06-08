34a04466fb759c3ef014ece6cdc5ffaf7b5f4573
dbene <bene.strz@googlemail.com>
2016-06-08 08:34:06 +0200

    updated UserDAO now using user views

--- src/UserDAO.js
+++ src/UserDAO.js
@@ -6,31 +6,31 @@ var UserDAO = function(connection) {
 };
 
 UserDAO.prototype.findByLogin = function(login, callbacks) {
-    this.daoHelper.find(this.connection.getFullUrl() + "destinationpathandquery" + login, callbacks);
+    this.daoHelper.find(this.connection.getFullUrl() + "_design/user/_view/userLogin?key=[%22" + encodeURI(login) + "%22]", callbacks);
 };
 
 UserDAO.prototype.findAll = function(callbacks) {
-    this.daoHelper.find(this.connection.getFullUrl() + "_design/types/_view/user", callbacks);
+    this.daoHelper.find(this.connection.getFullUrl() + "_design/user/_view/userALL", callbacks);
 };
 
 UserDAO.prototype.create = function(obj, callbacks) {
-    this.daoHelper.create(obj, this.connection.getFullUrl() + "destinationpath", callbacks);
+    this.daoHelper.create(obj, this.connection.getFullUrl() + "", callbacks);
 };
 
 UserDAO.prototype.update = function(obj, callbacks) {
-    this.daoHelper.update(obj, this.connection.getFullUrl() + "destinationpath", callbacks);
+    this.daoHelper.update(obj, this.connection.getFullUrl() + obj._id, callbacks);
 };
 
 UserDAO.prototype.createOrUpdate = function(obj, callbacks) {
     if (obj._id) {
-        this.update(obj, this.connection.getFullUrl() + "destinationpath", callbacks);
+        this.update(obj, this.connection.getFullUrl() + obj_id, callbacks);
     } else {
-        this.create(obj, this.connection.getFullUrl() + "destinationpath", callbacks);
+        this.create(obj, this.connection.getFullUrl() + "", callbacks);
     }
 };
 
 UserDAO.prototype.delete = function(obj, callbacks) {
-    this.daoHelper.delete(obj, this.connection.getFullUrl() + "destinationpath", callbacks);
+    this.daoHelper.delete(obj, this.connection.getFullUrl() + obj_id, callbacks);
 };
 
 exports.default = UserDAO;
