22961c887a4c5c91601be580293c368fdb90b915
dbene <bene.strz@googlemail.com>
2016-06-08 08:10:00 +0200

    index updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 var DaoManager = require("./DaoManager");
 var UserDAO = require("./UserDAO");
+var MessageDAO = require("./MessageDAO");
 
 var CouchDbApi = function() {};
 
 CouchDbApi.UserDAO = UserDAO;
+CouchDbApi.MessageDAO = MessageDAO;
 CouchDbApi.DaoManager = DaoManager;
 
 exports.default = CouchDbApi;
 module.exports = exports.default;
-
