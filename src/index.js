babde015d14105dae68c5b4c0b44f60edd061dd4
ozhurakovskaya <oxana.zhurakovskaya@fh-bielefeld.de>
2016-06-10 01:19:55 +0200

    PreferenceDAO implemented:
    function findAll(callbacks)
    function findByProfileId(id,callbacks)
    
    PreferenceDAO add to index.js
    add tests to test-app.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ var DaoManager = require("./DaoManager");
 var UserDAO = require("./UserDAO");
 var MessageDAO = require("./MessageDAO");
 var PictureDAO = require("./PictureDAO");
-
+var PreferenceDAO = require("./PreferenceDAO");
 var CouchDbApi = function() {};
 
 CouchDbApi.UserDAO = UserDAO;
 CouchDbApi.MessageDAO = MessageDAO;
 CouchDbApi.PictureDAO = PictureDAO;
 CouchDbApi.DaoManager = DaoManager;
-
+CouchDbApi.PreferenceDAO = PreferenceDAO;
 
 exports.default = CouchDbApi;
 module.exports = exports.default;
--- src/test-app.js
+++ src/test-app.js
@@ -4,28 +4,21 @@ var connSettings = require("./conn-settings.js");
 
 var dm = new CouchDbApi.DaoManager(connSettings);
 
-var userDao = dm.getDao(CouchDbApi.UserDAO);
+var preferenceDAO = dm.getDao(CouchDbApi.PreferenceDAO);
 
 var testAppDiv = document.getElementById("test-app");
 
 var callbacks = {
     success: function(data) {
-        testAppDiv.innerHTML = JSON.stringify(data);
+        testAppDiv.innerHTML += "<br>" + JSON.stringify(data)+ "<br><br>";
     },
     error: function(error) {
         testAppDiv.innerHTML = error;
     }
 };
+testAppDiv.innerHTML += "<p>findAll: </p><br>";
+preferenceDAO.findAll(callbacks);
 
-userDao.findAll(callbacks);
+testAppDiv.innerHTML += "<p>findByProfileId: ca5c2c9fb2d201991f8b6f06e62186d1</p><br>";
+preferenceDAO.findByProfileId("ca5c2c9fb2d201991f8b6f06e62186d1",callbacks );
 
-// var myUser = {
-//     _id: "ca5c2c9fb2d201991f8b6f06e62440e0",
-//     _rev: "1-72f83619c7de8f9d9517db3ab5e445fb",
-//     doctype: "user",
-//     login: "carmenelectra",
-//     password: "carmenelectra",
-//     role: 0
-// };
-//
-// userDao.delete(myUser);
