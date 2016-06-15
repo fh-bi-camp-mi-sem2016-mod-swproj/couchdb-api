f4c993a1eb1f1100f1ae2e640c19d9b5e52e72c8
Alexej Esau <alexej.esau@fh-bielefeld.de>
2016-06-15 14:19:38 +0200

    Add possibility to use promises
    The way the callbacks were consumed results fairly fast in unreadable
    and unmaintainable code.
    
    To avoid such code it is possible to use promises now.
    To achieve this the library Q (https://github.com/kriskowal/q)
    has been used.

--- src/DaoHelper.js
+++ src/DaoHelper.js
@@ -1,24 +1,31 @@
+var q = require("q");
+
 var DaoHelper = function() {};
 
 DaoHelper.prototype.find = function(dest, callbacks) {
+    var defer = q.defer();
+
     if (typeof $ === "function" && typeof $.ajax === "function") {
         $.ajax({
             url: dest,
             type: "GET",
             contentType: "application/json"
         }).success(function(data, textStatus, jqXHR) {
+            var jsonResponse = JSON.parse(data);
+            var rows = [];
+            for (var index = 0; index < jsonResponse.rows.length; index++) {
+                rows.push(jsonResponse.rows[index].value);
+            }
+
             if (callbacks && typeof callbacks.success === "function") {
-                var jsonResponse = JSON.parse(data);
-                var rows = [];
-                for (var index = 0; index < jsonResponse.rows.length; index++) {
-                    rows.push(jsonResponse.rows[index].value);
-                }
                 callbacks.success(rows);
             }
+            defer.resolve(rows);
         }).error(function(jqXHR, textStatus, errorThrown) {
             if (callbacks && typeof callbacks.error === "function") {
                 callbacks.error(errorThrown);
             }
+            defer.reject(errorThrown);
         });
     } else {
         fetch(dest, {
@@ -30,22 +37,29 @@ DaoHelper.prototype.find = function(dest, callbacks) {
         }).then(function(response) {
             return response.json();
         }).then(function(jsonResponse) {
+            var rows = [];
+            for (var index = 0; index < jsonResponse.rows.length; index++) {
+                rows.push(jsonResponse.rows[index].value);
+            }
+
             if (callbacks && typeof callbacks.success === "function") {
-                var rows = [];
-                for (var index = 0; index < jsonResponse.rows.length; index++) {
-                    rows.push(jsonResponse.rows[index].value);
-                }
                 callbacks.success(rows);
             }
+            defer.resolve(rows);
         }).catch(function(err) {
             if (callbacks && typeof callbacks.error === "function") {
                 callbacks.error(err);
             }
+            defer.reject(err);
         });
     }
+
+    return defer.promise;
 };
 
 DaoHelper.prototype.create = function(obj, dest, callbacks) {
+    var defer = q.defer();
+
     if (typeof $ === "function" && typeof $.ajax === "function") {
         $.ajax({
             url: dest,
@@ -60,10 +74,12 @@ DaoHelper.prototype.create = function(obj, dest, callbacks) {
             if (callbacks && typeof callbacks.success === "function") {
                 callbacks.success(JSON.parse(data));
             }
+            defer.resolve(JSON.parse(data));
         }).error(function(jqXHR, textStatus, errorThrown) {
             if (callbacks && typeof callbacks.error === "function") {
                 callbacks.error(errorThrown);
             }
+            defer.reject(errorThrown);
         });
     } else {
         fetch(dest, {
@@ -83,15 +99,21 @@ DaoHelper.prototype.create = function(obj, dest, callbacks) {
             if (callbacks && typeof callbacks.success === "function") {
                 callbacks.success(jsonResponse);
             }
+            defer.resolve(jsonResponse)
         }).catch(function(err) {
             if (callbacks && typeof callbacks.error === "function") {
                 callbacks.error(err);
             }
+            defer.reject(err);
         });
     }
+
+    return defer.promise;
 };
 
 DaoHelper.prototype.update = function(obj, dest, callbacks) {
+    var defer = q.defer();
+
     if (typeof $ === "function" && typeof $.ajax === "function") {
         $.ajax({
             url: dest,
@@ -102,10 +124,12 @@ DaoHelper.prototype.update = function(obj, dest, callbacks) {
             if (callbacks && typeof callbacks.success === "function") {
                 callbacks.success(JSON.parse(data));
             }
+            defer.resolve(JSON.parse(data));
         }).error(function(jqXHR, textStatus, errorThrown) {
             if (callbacks && typeof callbacks.error === "function") {
                 callbacks.error(errorThrown);
             }
+            defer.reject(errorThrown);
         });
     } else {
         fetch(dest, {
@@ -121,15 +145,21 @@ DaoHelper.prototype.update = function(obj, dest, callbacks) {
             if (callbacks && typeof callbacks.success === "function") {
                 callbacks.success(jsonResponse);
             }
+            defer.resolve(jsonResponse);
         }).catch(function(err) {
             if (callbacks && typeof callbacks.error === "function") {
                 callbacks.error(err);
             }
+            defer.reject(err);
         });
     }
+
+    return defer.promise;
 };
 
 DaoHelper.prototype.delete = function(obj, dest, callbacks) {
+    var defer = q.defer();
+
     if (typeof $ === "function" && typeof $.ajax === "function") {
         $.ajax({
             url: dest,
@@ -140,10 +170,12 @@ DaoHelper.prototype.delete = function(obj, dest, callbacks) {
             if (callbacks && typeof callbacks.success === "function") {
                 callbacks.success(JSON.parse(data));
             }
+            defer.resolve(JSON.parse(data));
         }).error(function(jqXHR, textStatus, errorThrown) {
             if (callbacks && typeof callbacks.error === "function") {
                 callbacks.error(errorThrown);
             }
+            defer.reject(errorThrown);
         });
     } else {
         fetch(dest, {
@@ -158,12 +190,16 @@ DaoHelper.prototype.delete = function(obj, dest, callbacks) {
             if (callbacks && typeof callbacks.success === "function") {
                 callbacks.success(jsonResponse);
             }
+            defer.resolve(jsonResponse);
         }).catch(function(err) {
             if (callbacks && typeof callbacks.error === "function") {
                 callbacks.error(err);
             }
+            defer.reject(err);
         });
     }
+
+    return defer.promise;
 };
 
 exports.default = DaoHelper;
